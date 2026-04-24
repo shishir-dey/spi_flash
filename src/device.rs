f6878801b5b52b36e37934f993b1191538d20684
Shishir Dey <shishirdey241097@gmail.com>
2026-04-24 15:35:18 +0530

    Add chip/sector/block erase methods

--- src/device.rs
+++ src/device.rs
@@ -188,4 +188,50 @@ impl<SPI: SpiInterface, CS: CsPin, Timer: Delay> SpiFlash<SPI, CS, Timer> {
         self.page_count = self.sector_count * 16;
         Ok(())
     }
+
+    pub fn erase_chip(&mut self) -> Result<(), SPI::SpiError> {
+        self.lock();
+        self.write_enable();
+        self.cs_drive(true);
+        self.spi.write(&[Command::ChipErase1 as u8]);
+        self.cs_drive(false);
+        self.wait_for_writing(100);
+        self.write_disable();
+        self.unlock();
+        Ok(())
+    }
+
+    pub fn erase_sector(&mut self, sector_number: u32) -> Result<(), SPI::SpiError> {
+        self.lock();
+        self.write_enable();
+        self.cs_drive(true);
+        self.spi.write(&[
+            Command::SectorErase4Add as u8,
+            ((sector_number * SECTOR_SIZE) >> 16) as u8,
+            ((sector_number * SECTOR_SIZE) >> 8) as u8,
+            (sector_number * SECTOR_SIZE) as u8,
+        ]);
+        self.cs_drive(false);
+        self.wait_for_writing(100);
+        self.write_disable();
+        self.unlock();
+        Ok(())
+    }
+
+    pub fn erase_block(&mut self, block_number: u32) -> Result<(), SPI::SpiError> {
+        self.lock();
+        self.write_enable();
+        self.cs_drive(true);
+        self.spi.write(&[
+            Command::BlockErase4Add as u8,
+            ((block_number * BLOCK_SIZE) >> 16) as u8,
+            ((block_number * BLOCK_SIZE) >> 8) as u8,
+            (block_number * BLOCK_SIZE) as u8,
+        ]);
+        self.cs_drive(false);
+        self.wait_for_writing(100);
+        self.write_disable();
+        self.unlock();
+        Ok(())
+    }
 }
